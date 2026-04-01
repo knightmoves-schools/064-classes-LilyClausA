55e4f1301e86ccf77106bab6b5012b41c35a7132
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-04-01 19:04:50 +0000

    ghui:grhurwhf

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+class Car {
+    constructor(name, year) {
+        this.name = name;
+        this.year = year;
+    }
+    age() {
+        let date = new Date ();return date.getFullYear() - this.year;
+    }
 
+}
+  
+let car = new Car ('toyota', 2006);
