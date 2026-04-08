bb286d99ad2c253de6d6c02409b368c899cc7a31
LilyClausA <lilcla1230@students.maquoketaschools.org>
2026-04-08 19:19:08 +0000

    hibby gibby

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 class person {
     constructor(firstName, lastName) {
-        this.firstNameme = Angaline;
-        this.lastName = whoopy;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
 }
+let guest = [
+      new person("Avangeline");
+new person("Andy");
+new person("Anastatia");
+]
+
+
 document.getElemrntById("result").innerHTML = person;
 
 //should define a class named Person that takes two parameters: firstName and lastName when it's created
 
-//should create a list named guests that contains three instances of the Person class
+//should create a list named guests 
+// that contains three instances of the Person class
